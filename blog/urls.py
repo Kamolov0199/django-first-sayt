d72a7c3d5f2ca9febfcd3d10a78bee85f0e25000
Your Name <you@example.com>
2022-11-08 16:36:03 +0500

    new

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,11 @@ from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('asos/', asosiy),
+    path('asosiy/', asosiy),
+    path('asosiy/cars1.html/', ferrary),
+    path('asosiy/cars2.html/', mersedes),
+    path('asosiy/cars3.html/', bmv),
+    path('asosiy/cars4.html/', toyota),
+    path('asosiy/cars5.html/', mers),
+    path('asosiy/cars6.html/', nissan)
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def asosiy(request):
     return render(request, 'index.html')
+
+def ferrary(request):
+    return render(request, 'cars1.html')
+
+def mersedes(request):
+    return render(request, 'cars2.html')
+
+def bmv(request):
+    return render(request, 'cars3.html')
+
+def toyota(request):
+    return render(request, 'cars4.html')
+
+def mers(request):
+    return render(request, 'cars5.html')
+
+def nissan(request):
+    return render(request, 'cars6.html')
